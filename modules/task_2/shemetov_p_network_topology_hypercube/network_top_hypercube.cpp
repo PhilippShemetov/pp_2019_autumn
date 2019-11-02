40c382d427afdfa5c2be7e9a7fc35d7ba2048617
PhilippShemetov <filshe2704@mail.ru>
2019-11-02 20:52:29 +0300

    Recoding hypercube.cpp

--- modules/task_2/shemetov_p_network_topology_hypercube/network_top_hypercube.cpp
+++ modules/task_2/shemetov_p_network_topology_hypercube/network_top_hypercube.cpp
@@ -1,7 +1,22 @@
 #include <mpi.h>
 #include "./network_top_hypercube.h"
 
-MPI_Comm getHypercube(int weidth,int height) {
+MPI_Comm getHypercube(int dimension) {
     int sizeProc;
+    int lengthHyperCube = 2;
     MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
+    int *ndims = new int[dimension];
+    for (int i = 0; i < dimension; i++)
+    {
+        ndims[i] = lengthHyperCube;
+    }
+    int pivot = 1;
+    for (int i = 0; i < dimension; i++)
+    {
+        pivot *= ndims[i];
+    }
+    if (sizeProc % pivot != 0)
+        return MPI_COMM_NULL;
+
+    MPI_Dims_create(sizeProc, dimension, ndims);
 }
