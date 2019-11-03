faf534d9d43ba3df5d035673e766807e2bcadfdb
PhilippShemetov <filshe2704@mail.ru>
2019-11-03 14:58:30 +0300

    Add one test and add code in Header files

--- modules/task_2/shemetov_p_network_topology_hypercube/main.cpp
+++ modules/task_2/shemetov_p_network_topology_hypercube/main.cpp
@@ -3,6 +3,27 @@
 #include <mpi.h>
 #include "./network_top_hypercube.h"
 
+TEST(Network_Top_Hypercube,Test_On_Equale_Size_Proc_And_Size_Node_Hypercube) {
+    int rankProc;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rankProc);
+    if (rankProc == 0) {
+        ASSERT_EQ(MPI_COMM_NULL, getHypercube(2));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
--- modules/task_2/shemetov_p_network_topology_hypercube/network_top_hypercube.cpp
+++ modules/task_2/shemetov_p_network_topology_hypercube/network_top_hypercube.cpp
@@ -1,22 +1,37 @@
 #include <mpi.h>
 #include "./network_top_hypercube.h"
 
-MPI_Comm getHypercube(int dimension) {
-    int sizeProc;
-    int sizeNodeHyperCube = 2;
-    MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
-    int *ndims = new int[dimension];
-    for (int i = 0; i < dimension; i++)
-    {
-        ndims[i] = sizeNodeHyperCube;
+MPI_Comm getHypercube(int numDims) {
+    if (numDims < 2) {
+        throw "The hypercube should be >=2d";
     }
-    int pivot = 1;
-    for (int i = 0; i < dimension; i++)
+    const int sizeNodeHyperCube = 2;
+    int sizeProc;
+    int reorder, pivot = 1;
+    MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
+    int *sizeOfDim = new int[numDims];
+    int* periods = new int[numDims];
+    for (int i = 0; i < numDims; i++)
     {
-        pivot *= ndims[i];
+        sizeOfDim[i] = sizeNodeHyperCube;
+    }
+    for (int i = 0; i < numDims; i++)
+    {
+        periods[i] = 1;
+    }
+    for (int i = 0; i < numDims; i++)
+    {
+        pivot *= sizeOfDim[i];
     }
     if (sizeProc % pivot != 0)
         return MPI_COMM_NULL;
+    
 
-    MPI_Dims_create(sizeProc, dimension, ndims);
+    //MPI_Dims_create(sizeProc, dimension, ndims);
+    MPI_Comm hypercube_comm;
+
+    MPI_Cart_create(MPI_COMM_WORLD, numDims, sizeOfDim, periods, reorder, &hypercube_comm);
+    
+    return hypercube_comm;
+
 }
