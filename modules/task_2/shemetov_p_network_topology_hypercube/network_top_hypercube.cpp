aebe598fe93cae25301b09e73df9ebdab8e08d05
PhilippShemetov <filshe2704@mail.ru>
2019-11-03 19:51:38 +0300

    add bool test in hypercube.cpp

--- modules/task_2/shemetov_p_network_topology_hypercube/network_top_hypercube.cpp
+++ modules/task_2/shemetov_p_network_topology_hypercube/network_top_hypercube.cpp
@@ -1,12 +1,13 @@
 #include <mpi.h>
 #include "./network_top_hypercube.h"
+#include<iostream>
 
 MPI_Comm getHypercube(int numDims) {
     if (numDims < 2) {
         throw "The hypercube should be >=2d";
     }
     const int sizeNodeHyperCube = 2;
-    int sizeProc;
+    int sizeProc,rankProc;
     int reorder = 1, pivot = 1;
     MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
     int *sizeOfDim = new int[numDims];
@@ -31,7 +32,39 @@ MPI_Comm getHypercube(int numDims) {
     MPI_Comm hypercube_comm;
 
     MPI_Cart_create(MPI_COMM_WORLD, numDims, sizeOfDim, periods, reorder, &hypercube_comm);
-    
+    /*MPI_Cart_coords(hypercube_comm,rankProc,numDims,coord);
+    std::cout << " Rank " << rankProc << " coordinates are " 
+        << coord[0] << coord[1] << coord[2] << std::endl;*/
+
+
     return hypercube_comm;
 
+}
+
+bool thisIsHypercube(MPI_Comm test_comm,int numDims) {
+    const int sizeNodeHyperCube = 2;
+    int countDis, status;
+    int* sizeOfDim = new int[numDims];
+    int* periods = new int[numDims];
+    int* coord = new int[numDims];
+    MPI_Cartdim_get(test_comm, &countDis);
+    if (numDims != countDis) {
+        return false;
+    }
+    MPI_Topo_test(test_comm,&status);
+    if (status != MPI_CART) {
+        return false;
+    }
+    for (int i = 0; i < numDims; i++)
+    {
+        sizeOfDim[i] = sizeNodeHyperCube;
+    }
+    MPI_Cart_get(test_comm, numDims,sizeOfDim, periods, coord);
+    for (int i = 0; i < numDims; i++)
+    {
+        if (periods[i] != 1) {
+            return false;
+        }
+    }
+    return true;
 }
